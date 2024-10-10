76143ecf8086262d17a1502f6144aef390a9eccf
fagote <leofagot@hotmail.com>
2024-10-10 16:18:08 -0300

    Logo das empresas atualizadas, nova migration criada na tabela files para adicionar as colunas 'ativo' e 'aprovacao'.

---
+++ database/migrations/2024_10_10_173657_update_files_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Schema::table, pois estou alterando uma tabela existente.
+        Schema::table('files', function(Blueprint $table){
+            $table->boolean('ativo');
+            $table->unsignedInteger('aprovacao')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('files', function (Blueprint $table){
+            $table->dropColumn('ativo');
+            $table->dropColumn('aprovacao');
+        });
+    }
+};
